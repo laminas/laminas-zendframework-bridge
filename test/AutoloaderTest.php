bf0e7ce6239f85d55c0156a7f2fc69eec94370c1
webimpress <contact@webimpress.com>
2019-04-11 22:37:43 +0100

    Added unit tests to check if autoloader works correctly

---
+++ test/AutoloaderTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace LaminasTest\ZendFrameworkBridge;
+
+use PHPUnit\Framework\TestCase;
+
+class AutoloaderTest extends TestCase
+{
+    public function classProvider()
+    {
+        return array(
+            array('\Zend\Main'),
+            array('\Zend\Expressive\Expressive'),
+            array('\Zend\Expressive\Authentication\Authentication'),
+            array('\Zend\Expressive\Authentication\ZendAuthentication\AuthenticationAdapter'),
+            array('\Zend\Expressive\Router\Router'),
+            array('\Zend\Expressive\Router\ZendRouter\RouterAdapter'),
+            array('Zend\ProblemDetails\ProblemDetails'),
+        );
+    }
+
+    /**
+     * @dataProvider classProvider
+     * @param string $className
+     */
+    public function testLegacyClassIsAliasToLaminas($className)
+    {
+        self::assertTrue(class_exists($className));
+    }
+}
---
+++ test/classes.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Laminas {
+    class Main {}
+}
+
+namespace Expressive {
+    class Expressive {}
+}
+
+namespace Expressive\Authentication {
+    class Authentication {}
+}
+
+namespace Expressive\Authentication\LaminasAuthentication {
+    class AuthenticationAdapter {}
+}
+
+namespace Expressive\Router {
+    class Router {}
+}
+
+namespace Expressive\Router\LaminasRouter {
+    class RouterAdapter {}
+}
+
+namespace Expressive\ProblemDetails {
+    class ProblemDetails {}
+}
